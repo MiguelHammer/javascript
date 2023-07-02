ce3ccab3dba5a5e8f4e307d8b12de12d721ac2e1
Miguel Da Silva Hammer <138016644+MiguelHammer@users.noreply.github.com>
2023-07-02 16:40:11 -0300

    Nova Aula sobre função

---
+++ aulas/a016/funçao01.js
@@ -0,0 +1,22 @@
+function parimp(n) {
+    if (n % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+var res = console.log(parimp(10))
+
+/*
+function parimpa(n) {
+    console.log(`Você digitou: ${n}`)
+    if (n % 2 == 0) {
+        console.log(`Esse número é Par`)
+    } else if (n % 2 == 1){
+        console.log(`Esse número é Impar`)
+    }
+}
+
+parimpa(92)
+*/
